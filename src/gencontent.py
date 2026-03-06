671be46a2a7dde9b099c03ef32202f86bcce76f9
Kay Murray <kay@Kays-MacBook-Pro.local>
2026-03-06 10:41:46 -0700

    made usable for github pages

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,7 +10,7 @@ def extract_title(markdown):
 			return main_heading
 	raise Exception("no h1 found")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         from_content = f.read()
@@ -19,18 +19,20 @@ def generate_page(from_path, template_path, dest_path):
     content = markdown_to_html_node(from_content).to_html()
     title = extract_title(from_content)
     template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    template_content = template_content.replace("href=\"/", f"href=\"{base_path}")
+    template_content = template_content.replace("src=\"/", f"src=\"{base_path}")
     if os.path.dirname(dest_path):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as h:
         h.write(template_content)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, base_path):
     files = os.listdir(dir_path_content)
     for item in files:
         itempath = os.path.join(dir_path_content, item)
         destpath = os.path.join(dest_dir_path, item)
         if os.path.isfile(itempath) and itempath.endswith(".md"):
             destpath = destpath.replace(".md", ".html")
-            generate_page(itempath, template_path, destpath)
+            generate_page(itempath, template_path, destpath, base_path)
         else:
-            generate_pages_recursive(itempath, template_path, destpath)
+            generate_pages_recursive(itempath, template_path, destpath, base_path)
--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
+import sys
 from textnode import TextNode, TextType
 from copystatic import copy_static
 from gencontent import generate_pages_recursive
 
 def main():
-	copy_static("./static", "./public", True)
-	generate_pages_recursive("content", "template.html", "public")
+	basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+	copy_static("./static", "./docs", True)
+	generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
