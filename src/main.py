4b1742d236ad63859aee0b58f6ab4638e76e0e19
Kay Murray <kay@Kays-MacBook-Pro.local>
2026-03-06 08:19:48 -0700

    adjusted main.py and main.sh to serve on localhost

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from textnode import TextNode, TextType
 from copystatic import copy_static
+from gencontent import generate_page
 
 def main():
 	copy_static("./static", "./public", True)
+	generate_page("content/index.md", "template.html", "public/index.html")
 
 main()
