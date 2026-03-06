762039c6e2cf1b1c22051e4884339c4ea25ff0ab
Kay Murray <kay@Kays-MacBook-Pro.local>
2026-03-06 08:03:32 -0700

    added generate_page

--- src/gencontent.py
+++ src/gencontent.py
@@ -1,3 +1,7 @@
+import os
+
+from markdown_blocks import markdown_to_html_node
+
 def extract_title(markdown):
 	lines = markdown.splitlines()
 	for line in lines:
@@ -5,3 +9,17 @@ def extract_title(markdown):
 			main_heading = line.removeprefix("# ").strip()
 			return main_heading
 	raise Exception("no h1 found")
+
+def generate_page(from_path, template_path, dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        from_content = f.read()
+    with open(template_path) as g:
+        template_content = g.read()
+    content = markdown_to_html_node(from_content).to_html()
+    title = extract_title(from_content)
+    template_content = template_content.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    if os.path.dirname(dest_path):
+        os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "w") as h:
+        h.write(template_content)
